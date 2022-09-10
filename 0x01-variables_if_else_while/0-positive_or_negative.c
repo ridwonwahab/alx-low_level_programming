a1312329b2aecf762acd9aa080f3c856b43bc72e
ridwonwahab <ridwonwahab@gmail.com>
2022-09-09 20:25:32 -0700

    task 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,7 +18,7 @@ if (n > 0)
 
 {
 
-printf ("%d is positive\n", n);
+printf("%d is positive\n", n);
 
 }
 
@@ -26,15 +26,14 @@ else if (n == 0)
 
 {
 
-printf ("%d is zero\n",n);
+printf("%d is zero\n", n);
 
 }
 
 else
 
 {
-
-        printf ("%d is negative\n",n);
+printf ("%d is negative\n", n);
 
 }
 
