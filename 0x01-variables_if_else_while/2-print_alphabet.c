595a357bb29fac5bc21918287c6a974c0c4260e3
ridwonwahab <ridwonwahab@gmail.com>
2022-09-10 09:19:19 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Return: 0
+ */
+int main(void)
+{
+char englishAlpha = 'a';
+
+while (englishAlpha <= 'z')
+{
+putchar(englishAlpha);
+englishAlpha++;
+}
+putchar('\n');
+return (0);
+}
