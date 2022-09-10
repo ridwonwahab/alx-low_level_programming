5098dc78174752ad14f72020ff773ed89fcf6121
ridwonwahab <ridwonwahab@gmail.com>
2022-09-10 06:51:02 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,43 +4,41 @@
 
 #include <time.h>
 
-/* more headers goes there */
 
-
-
-/** betty style doc for function main goes there
- * main method
- */
+/**
+ * main - this is the main method
+ * Return: 0 if all is fine
+* betty style doc for function main goes there
+* main method
+* RETURN
+*/
 
 int main(void)
 
 {
 
-  int n;
-  int ballance;
+int n;
+int ballance;
 
+srand(time(0));
 
-  srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-  n = rand() - RAND_MAX / 2;
+/* your code goes there */
+ballance = n % 10;
+if (ballance > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, ballance);
+}
+else if (ballance == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, ballance);
 
-  /* your code goes there */
-  
-  ballance = n % 10;
-  if (ballance > 5)
-    {
-      printf("Last digit of %d is %d and is greater than 5\n", n, ballance);
-    }
-  else if (ballance == 0)
-    {
-      printf("Last digit of %d is %d and is 0\n", n, ballance);
-    }
-  else if (ballance < 6 && !(ballance == 0) )
-    {
-      printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ballance);
-    }
+else if (ballance < 6 && !(ballance == 0))
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ballance);
+}
 
-
-  return (0);
+return (0);
 
 }
