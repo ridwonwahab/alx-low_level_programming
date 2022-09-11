d315e8011020a107aa4764394222765369f76a35
ridwonwahab <ridwonwahab@gmail.com>
2022-09-11 07:55:47 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print z to a
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+char ZtoA;
+for (ZtoA = 'z'; ZtoA >= 'a'; ZtoA--)
+{
+putchar(ZtoA);
+}
+putchar('\n');
+
+
+return (0);
+}
