a6fbf9b0997c3b7b06767c54c57fa6ed46a4fd03
ridwonwahab <ridwonwahab@gmail.com>
2022-09-10 19:49:26 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 9
+ * Return: 0 if all is good
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+printf("%d", i);
+i++;
+}
+printf("\n");
+
+return (0);
+}
