b5b555d9858f0f3b7b4e9520208f471f20ee4935
ridwonwahab <ridwonwahab@gmail.com>
2022-09-11 07:43:40 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers using putchar
+ * Return : 0 if runs
+ **/
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+
+return (0);
+}
