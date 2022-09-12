e7b7988eb5c9b8c763c471c76c7adc2324145a30
ridwonwahab <ridwonwahab@gmail.com>
2022-09-11 22:56:18 -0700

    task 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,35 +7,32 @@
 
 int main(void)
 {
-  int x, y, z;
+int x, y, z;
 
-  for (x = 0; x <= 7; x++)
-    {
-      for (y = 1; y <= 8; y++)
-	{
+for (x = 0; x <= 7; x++)
+{
+for (y = 1; y <= 8; y++)
+{
 
-	  for (z = 2; z <= 9; z++)
-	    {
-	      if (z > y && y > x)
-		{
-		  putchar ('0' + x);
-	      putchar ('0' + y);
-	      putchar ('0' + z);
-	      putchar (',');
-	      putchar (' ');
-	    }
-	    }
-	}
-    }
+for (z = 2; z <= 9; z++)
+{
+if (z > y && y > x)
+{
+putchar ('0' + x);
+putchar ('0' + y);
+putchar ('0' + z);
+if (!(x == 7 && y == 8 && z == 9))
+{
+putchar (',');
+putchar (' ');
+}
+}
+}
+}
+}
 
-  putchar ('\n');
-
-
-
-
-
+putchar ('\n');
 
 
-
-  return (0);
-    }
+return (0);
+}
