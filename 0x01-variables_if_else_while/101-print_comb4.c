22c556ffd8418b9712d1efc1cdda28785d279a24
ridwonwahab <ridwonwahab@gmail.com>
2022-09-11 22:46:03 -0700

    task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+* main - printing 3 combinations
+* Return: 0 if runs successfully
+*/
+
+int main(void)
+{
+  int x, y, z;
+
+  for (x = 0; x <= 7; x++)
+    {
+      for (y = 1; y <= 8; y++)
+	{
+
+	  for (z = 2; z <= 9; z++)
+	    {
+	      if (z > y && y > x)
+		{
+		  putchar ('0' + x);
+	      putchar ('0' + y);
+	      putchar ('0' + z);
+	      putchar (',');
+	      putchar (' ');
+	    }
+	    }
+	}
+    }
+
+  putchar ('\n');
+
+
+
+
+
+
+
+
+  return (0);
+    }
