c8c7d415b9cb1f9179035a5cdbf2eaaed45aab14
ridwonwahab <ridwonwahab@gmail.com>
2022-09-11 20:29:42 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - main function
+* Return: 0  if all is well
+*/
+
+int main(void)
+{
+int i;
+for (i = 0; i < 8; i++)
+{
+putchar('0' + i);
+putchar(',');
+putchar(' ');
+}
+putchar('0' + 9);
+
+return (0);
+}
