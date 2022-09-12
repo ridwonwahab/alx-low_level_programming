745152c63f1b1ed5f5a5a2d53821c3364c63b38b
ridwonwahab <ridwonwahab@gmail.com>
2022-09-11 20:38:24 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,16 @@
 int main(void)
 {
 int i;
-for (i = 0; i <= 8; i++)
+for (i = 0; i <= 9; i++)
 {
 putchar('0' + i);
+if (i <= 8)
+{   
 putchar(',');
 putchar(' ');
 }
-putchar('0' + 9);
+}
+putchar('\n');
 
 return (0);
 }
