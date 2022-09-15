c4783a01c848a829298738f10900392e653796b7
ridwonwahab <ridwonwahab@gmail.com>
2022-09-15 08:59:44 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+
+
+/**
+
+ * main - tests function that prints if integer is positive or negative
+
+ * Return: 0
+
+ */
+
+
+
+int main(void)
+
+{
+
+	int i;
+
+i = 98;
+
+        positive_or_negative(i);
+
+
+
+        return (0);
+
+}
+
+void positive_or_negative(int i)
+{
+	if (i > 0){
+		printf("%d is positive", i);
+	}
+	else if(i == 0){
+		printf("%d is zero", i);
+		       }
+	else {
+		printf("%d is negative", i);
+	}
+
+	printf("\n");
+}
