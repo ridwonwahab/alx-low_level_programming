b4a1596b7ae6a7395d4fe9a033f8de8a0208f2e8
ridwonwahab <ridwonwahab@gmail.com>
2022-09-15 23:52:13 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -19,27 +19,3 @@ positive_or_negative(i);
 return (0);
 
 }
-
-
-/**
- * positive_or_negative - funtion
- * @i: to be tested
- * Return: void
- */
-void positive_or_negative(int i)
-{
-	if (i > 0)
-	{
-		printf("%d is positive", i);
-	}
-	else if (i == 0)
-	{
-		printf("%d is zero", i);
-	}
-	else
-	{
-		printf("%d is negative", i);
-	}
-
-	printf("\n");
-}
