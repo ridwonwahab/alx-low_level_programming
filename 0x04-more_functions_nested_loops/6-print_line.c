a8027cbe172b9d1f75e86a189120768777e119c3
ridwonwahab <ridwonwahab@gmail.com>
2022-09-16 20:11:49 -0700

    task

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,9 @@
 #include "main.h"
 
 /**
-
  * print_line - print a straight line
-
- *
-
  * @n: is the number of times the _ character
-
  *     should be printed
-
  */
 
 void print_line(int n)
