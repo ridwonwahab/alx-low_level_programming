1662de10b7f6c94fe0cb52df71e12ce0441f6da0
ridwonwahab <ridwonwahab@gmail.com>
2022-09-16 19:22:53 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* mul - multiply
+*@a: first para
+*@b: second parm
+* Return: multiplication of first and second param
+*/
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,6 +10,10 @@ int _isupper(int c);
 
 int _isdigit(int c);
 
+int mul(int a, int b);
+
+
+
 
 
 #endif /* MAIN_H */
