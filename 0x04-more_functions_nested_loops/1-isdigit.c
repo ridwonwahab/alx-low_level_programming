6fcd14e4dfba83b6b972a72fa16199fbdbbea38a
ridwonwahab <ridwonwahab@gmail.com>
2022-09-16 19:13:14 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isdigit - checks maybe the passed value is a digit 0-9
+*@c: the passed value
+* Return: 1 if true 0 if false
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+
+
+
+	return (0);
+}
