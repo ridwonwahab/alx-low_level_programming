55f0eb35487c843688cbfdba50f758287255d820
ridwonwahab <ridwonwahab@gmail.com>
2022-09-19 19:29:34 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+This is a project on pointers and array.
+
+This project is the fifth sub project under alx low level programming
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
+/**
+ * _putchar - prints the given char
+ *@c: the char to be printed
+ *Return
+ */
+
+int _putchar(char c);
+
+
+void reset_to_98(int *n);
