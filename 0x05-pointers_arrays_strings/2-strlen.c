2fa9019fadc63faa55ce809b3691ae22b556a0b5
ridwonwahab <ridwonwahab@gmail.com>
2022-09-19 20:30:12 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_strlen - retutns the lenght of string
+ *@s: string which the lenght is to be found
+ *Return: lenght if the string
+ */
+int _strlen(char *s)
+{
+	int lenght = 0;
+
+	while (s[lenght]x)
+	{
+		lenght += 1;
+		lenght++;
+	}
+	return (lenght);
+}
