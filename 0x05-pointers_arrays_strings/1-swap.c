9bdbd3566e86210ff98bff6b4f9ab176d30b7e78
ridwonwahab <ridwonwahab@gmail.com>
2022-09-19 19:56:44 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *swap_int - this method swaps the values of two ints passed to it
+ *@a: the first int passed
+ *@b: the second int passed
+ */
+void swap_int(int *a, int *b)
+{
+
+	int z;
+
+	z = *a;
+	*a = *b;
+	*b = z;
+		}
