c35c466494fd51d701d8dba2920e537c028a97d1
ridwonwahab <ridwonwahab@gmail.com>
2022-09-28 21:58:02 -0700

    task 0 - 5

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+ void _putchar(char c);
+void _puts_recursion(char *s);
+int _sqrt_recursion(int n);
+int _pow_recursion(int x, int y);
+int factorial(int n);
+int _strlen_recursion(char *s);
+void _print_rev_recursion(char *s);
