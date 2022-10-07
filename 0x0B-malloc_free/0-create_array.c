b194877c757fcabb4087e5ef523881217922565e
ridwonwahab <ridwonwahab@gmail.com>
2022-10-06 20:58:14 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,17 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	int i;
 
-	while(1)
+	s = malloc (size * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	i=0;
+		while(i < size)
+		{
 		s[i] = c;
+		i++;
+		}
 	return (s);
 }
