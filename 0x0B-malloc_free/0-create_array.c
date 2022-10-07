55f7b14a4fd8c8c28f125a763c6815301caa56b7
ridwonwahab <ridwonwahab@gmail.com>
2022-10-06 20:43:32 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,8 @@
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+
+	while(true)
+		s[i] = c;
+	return (s);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,13 @@
+#idndefine
+#define
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+
+
+
+
+
+
+#enfid
