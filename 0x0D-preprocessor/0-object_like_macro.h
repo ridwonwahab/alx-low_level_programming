c850db557ba62fe3ed05ef513a0f1734fd775f12
ridwonwahab <ridwonwahab@gmail.com>
2022-10-11 06:48:24 -0700

    task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef 0_-_OBJECT_LIKE_MACRO_H
-#define 0_-_OBJECT_LIKE_MACRO_H
+#ifndef _OBJECT_LIKE_MACRO_H
+#define _OBJECT_LIKE_MACRO_H
 
 #include SIZE 1024
 
-#endif  /* _OBJECT_LIKE_MACRO_H
+#endif  /* _OBJECT_LIKE_MACRO_H */
